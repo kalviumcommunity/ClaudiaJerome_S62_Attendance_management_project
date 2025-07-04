8a2c313c59beaecb3c81b96956112ee078afb8ab
claudiajerome07 <claudiajerome07@gmail.com>
2025-07-04 17:09:29 +0530

    day3 code

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -1,13 +1,19 @@
 package com.school;
 
 public class Course {
-    String courseId; // e.g., "CS101"
+    private static int courseIdCounter=101;
+    int courseId; // e.g., "CS101"
     String courseName;
 
-    public void setDetails(String id, String cName) {
-        this.courseId = id;
-        this.courseName = cName;
+    public Course(String courseName) {
+        this.courseId=courseIdCounter++;
+        this.courseName = courseName;
     }
+
+    // public void setDetails(int id, String cName) {
+    //     this.courseId = id;
+    //     this.courseName = cName;
+    // }
 
     public void displayDetails() {
         System.out.println("Course ID: " + this.courseId + ", Name: " + this.courseName);
--- src/com/school/Student.java
+++ src/com/school/Student.java
@@ -1,13 +1,19 @@
 package com.school;
 
 public class Student {
+    private  int nextStudentIdCounter=1;
     int studentId;
     String name;
 
-    public void setDetails(int id, String studentName) {
-        this.studentId = id; // using 'this' for clarity
-        this.name = studentName;
+    public Student(String name) {
+        this.studentId = nextStudentIdCounter++; // using 'this' to refer to the instance variable
+        this.name=name;
     }
+
+    // public void setDetails(int id, String studentName) {
+    //     this.studentId = id; // using 'this' for clarity
+    //     this.name = studentName;
+    // }
 
     public void displayDetails() {
         System.out.println("Student ID: " + this.studentId + ", Name: " + this.name);
